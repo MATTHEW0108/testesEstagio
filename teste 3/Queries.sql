42329c68978f80f48fd87437df7a75d549da62f3
Matheus Ribeiro <matheusribeiroadelima2@gmail.com>
2023-05-25 22:06:41 -0400

    atualização 2505

--- teste 3/Queries.sql
+++ teste 3/Queries.sql
@@ -1,7 +1,11 @@
+use testes;
+
+SET GLOBAL local_infile=1;
+
 CREATE TABLE ans (
-	rAns int,
-    cnpj varchar(14),
-    razSoc varchar (100),
+	rAns varchar (10),
+    cnpj varchar(20),
+    razSoc varchar (150),
     nomFan varchar (100),
 	modal varchar (50),
     logr varchar (100),
@@ -9,14 +13,32 @@ CREATE TABLE ans (
     compl varchar (100),
     bairro varchar (50),
     cidade varchar (50),
-    uf varchar(2),
+    uf varchar(5),
     cep varchar(10),
-    ddd int,
-    telefone varchar(10),
+    ddd varchar(10),
+    telefone varchar(20),
     fax varchar(15),
     endEl varchar(50),
     represent varchar (60),
     cargoR varchar (60),
-    dataReg varchar (15),
+    dataReg varchar (25),
     primary key (rAns)
 );
+
+
+create table conj_dados (
+	org varchar (60),
+    nome varchar (75),
+    descr varchar (500),
+    tags varchar (300),
+    qtdRec int,
+    qtdReu int, 
+    qtdDownl int,
+    qtdSeg int
+);
+
+show variables like "secure_file_priv";
+
+load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Relatorio_cadop.csv' into table ans CHARACTER SET latin1 fields terminated by ';' enclosed by '' lines terminated by '\n'ignore 1 rows ;
+
+select * from  ans
